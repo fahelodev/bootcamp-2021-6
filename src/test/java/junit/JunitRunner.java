46444ab09077549dff98d2dab67b679927575eeb
fahelodev <fahelo.dev@gmail.com>
2021-12-16 12:35:02 -0300

    init bootcamp 6 :: termanada libreria Junit4

---
+++ src/test/java/junit/JunitRunner.java
@@ -0,0 +1,10 @@
+package junit;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AssertTest.class,CalculadoraTest.class})
+public class JunitRunner {
+}
