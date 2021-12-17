2c2d5eee35ab5566f31072274b99581138f14596
TSOFT\Isaac.Romero <isaac.romero@tsoftlatam.com>
2021-12-17 11:35:07 -0300

    Archivos de isaac romero semana JUnit

---
+++ src/test/java/katas/iromero/JUnitRunner.java
@@ -0,0 +1,11 @@
+package iromero;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+//se llaman .class ya que se la hace llamado al archivo en tiempo de ejecucion
+@Suite.SuiteClasses({ReverseWordsTest.class, AlternatingCaseTest.class, BuyCarTest.class, DeclareWinnerTest.class})
+public class JUnitRunner {
+
+}
