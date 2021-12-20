01647003c9802115f08a48d2dc988acb7ca46016
vargas44 <ml_44@live.com.ar>
2021-12-20 09:09:32 -0300

    Agrego archivos bootcamp de Rodrigo Vargas

---
+++ src/main/java/katas/rvargas/Fighter.java
@@ -0,0 +1,13 @@
+package katas.rvargas;
+
+public class Fighter {
+
+        public String name;
+        public int health, damagePerAttack;
+        public Fighter(String name, int health, int damagePerAttack) {
+            this.name = name;
+            this.health = health;
+            this.damagePerAttack = damagePerAttack;
+        }
+    }
+
