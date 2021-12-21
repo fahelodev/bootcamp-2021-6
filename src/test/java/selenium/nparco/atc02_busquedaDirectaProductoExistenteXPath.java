dcd3bf298010d98ccdc953c7dfe528553cf6c24c
Nicolas Parco <Nicolas.Parco@tsoftglobal.com>
2021-12-21 19:03:04 -0300

    Se agrego casos de prueba atc01 & atc02

---
+++ src/test/java/selenium/nparco/atc02_busquedaDirectaProductoExistenteXPath.java
@@ -0,0 +1,44 @@
+package selenium.nparco;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class atc02_busquedaDirectaProductoExistenteXPath {
+    public static void main(String[] args) throws InterruptedException
+    {
+        WebDriver driver;
+
+        //Inicialización del WebDriver con Chrome
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        //Inicialización del WebDriver con Firefox
+//        WebDriverManager.firefoxdriver().setup();
+//        driver = new FirefoxDriver();
+
+        //Cargar la página
+        driver.get("http://automationpractice.com/");
+
+        driver.findElement(By.xpath("//*[@id='search_query_top']")).sendKeys("printed chiffon dress");
+        driver.findElement(By.xpath("//*[@id='searchbox']/button")).click();
+
+        List<WebElement> results = driver.findElements(By.xpath("//li[contains(@class,'ajax_block_product')]"));
+
+        System.out.println(((List<?>) results).size());
+        if (results.size()>=2){
+            System.out.println("Prueba Exitosa");
+        }else{
+            System.out.println("Prueba Fallida");
+        }
+
+
+        //Thread.sleep(2000);
+        //driver.close();
+
+    }
+}
