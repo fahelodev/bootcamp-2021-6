6f2ce6708d6c5adfd6881fc93880c19d32e14a53
Marcos Patricio Vargas Sanhueza <Marcos.Vargas@tsoftglobal.com>
2021-12-21 18:27:33 -0300

    se agrega clase atc01_Busqueda_Palabras_Clave

--- src/test/java/selenium/mvargas/atc01_Busqueda_Palabras_Claves.java
+++ src/test/java/selenium/mvargas/atc01_Busqueda_Palabras_Claves.java
@@ -1,9 +1,5 @@
 package selenium.mvargas;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class atc00_Busqueda_Palabras_Claves {
+public class atc01_Busqueda_Palabras_Claves {
 }
