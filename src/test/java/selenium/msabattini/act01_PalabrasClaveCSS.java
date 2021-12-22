e423ee02be27d69cec3c932546d9928e488a332f
Martin Sabattini <Martin.Sabattini@tsoftglobal.com>
2021-12-21 21:23:22 -0300

    Se cambio el nombre de la clase

--- src/test/java/selenium/msabattini/act01_PalabrasClaveCSS.java
+++ src/test/java/selenium/msabattini/act01_PalabrasClaveCSS.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class act01_ParabolasClaveCSS {
+public class act01_PalabrasClaveCSS {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver;
 
