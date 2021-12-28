3afa1bf7e4649f2d28d22ba6d2d5d02c963bae40
EmilianoTCS <emi.vega84@gmail.com>
2021-12-28 16:55:08 -0300

    Todo listo para trabajar

---
+++ src/test/java/Test_Falabella/ATC_Alojamientos.java
@@ -0,0 +1,46 @@
+package Test_Falabella;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ATC_Alojamientos {
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.out.println("Setup necesario antes de Instanciar");
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void init() {
+        System.out.println("instanciar");
+        driver = new ChromeDriver();
+        //Page practice
+        driver.get("https://www.viajesfalabella.cl/hoteles/ ");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void close () {
+        if (driver != null) {
+            System.out.println("Close");
+            driver.close();
+        }
+
+    }
+
+    @AfterClass
+    public static void closeAll() {
+        System.out.println("closeAll :: Cerrar otras conexiones que fueron utilizadas en el test");
+    }
+}
