965297ada381b551e15f0ab739a3928d05ed4cd2
EmilianoTCS <emi.vega84@gmail.com>
2021-12-31 18:35:40 -0300

    Agregados test 1 y 2 de alojamientos

--- src/test/java/Test_Falabella/ATC_Alojamientos.java
+++ src/test/java/Test_Falabella/ATC_Alojamientos.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -30,6 +31,43 @@ public class ATC_Alojamientos {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
+    @Test
+    public void TC001_BuscarAlojamientoSinFechaNacional(){
+        //buscar Arica
+    driver.findElement(By.cssSelector("input.input-tag.sbox-main-focus.sbox-destination.sbox-primary.undefined")).sendKeys("Arica");
+    driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    //Click en el autocompletado
+    driver.findElement(By.cssSelector("span.item-text")).click();
+    //Seleccionar Checkbox
+    driver.findElement(By.cssSelector("i.checkbox-check.sbox-3-icon-checkmark.-mr1")).click();
+    //Buscar
+    driver.findElement(By.cssSelector("div.sbox-button-container")).click();
+    //Comparacion con Asserts
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+        String Result = driver.findElement(By.cssSelector("h2.eva-3-h2.tag-text-heading")).getText();
+        Assert.assertEquals("Hoteles destacados en Arica", Result);
+    }
+    @Test
+    public void TC002_FiltrarPorEstrellas(){
+        driver.findElement(By.cssSelector("input.input-tag.sbox-main-focus.sbox-destination.sbox-primary.undefined")).sendKeys("Arica");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        //Click en el autocompletado
+        driver.findElement(By.cssSelector("span.item-text")).click();
+        //Seleccionar Checkbox
+        driver.findElement(By.cssSelector("i.checkbox-check.sbox-3-icon-checkmark.-mr1")).click();
+        //Buscar
+        driver.findElement(By.cssSelector("div.sbox-button-container")).click();
+        //Click en estrellas
+        driver.findElement(By.xpath("//body[1]//app-root//offers-filters//eva-tooltip[4]")).click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        //Seleccionar una estrella
+        driver.findElement(By.xpath("//body[1]//app-root//offers-filters//eva-tooltip[4]//offers-filters-content[1]/div[1]/eva-checkbox[3]/span[1]/label[1]/span[1]/em[1]")).click();
+        driver.findElement(By.cssSelector("a.eva-3-btn.-md.-primary")).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        //Seleccionar primera opcion
+        driver.findElement(By.cssSelector("[title='Amaru Express'] .swiper-slide-active > media-picture > div > div > img")).click();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    }
     @After
     public void close () {
         if (driver != null) {
