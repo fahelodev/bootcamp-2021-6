9e9b150e58e8da344a23834fdae6660961e9199f
Francisco Marfull Pizarro <Francisco.Marfull@tsoftglobal.com>
2022-01-06 15:41:09 -0300

    Sketch inicial equipo 3 agregado.

---
+++ src/test/java/pom/mentoria/equipo3/base/SeleniumBase.java
@@ -0,0 +1,39 @@
+package pom.mentoria.equipo3.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class SeleniumBase {
+
+    //Atributos
+    WebDriver driver;
+
+    public SeleniumBase(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Metodos Wrapper - Envoltorios
+
+    //retorno ->  en su declaracion debe indicar que Objeto retorna
+    public WebElement encontrarElementoWeb(By localizador){
+        return driver.findElement(localizador);
+    }
+
+    public List<WebElement> encontrarElementosWeb(By localizador){
+        return driver.findElements(localizador);
+    }
+
+    //accion -> void
+    public void obtenerUrl(String URL){
+        driver.get(URL);
+    }
+
+    //accion -> void
+    public void clickear(By localizador){
+        encontrarElementoWeb(localizador).click();
+    }
+
+}
