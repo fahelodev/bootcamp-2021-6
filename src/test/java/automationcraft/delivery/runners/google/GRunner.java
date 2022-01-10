49eea997b80620c42d1e4f3dd6d21933939b85aa
fahelodev <fahelo.dev@gmail.com>
2022-01-10 11:37:07 -0300

    init bootcamp 6 :: faregtsoft demo lista para agregar funcionalidad selenium

---
+++ src/test/java/automationcraft/delivery/runners/google/GRunner.java
@@ -0,0 +1,22 @@
+package automationcraft.delivery.runners.google;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = {"src/test/java/automationcraft/testcreation/google/features"},
+        glue = {"automationcraft.testcreation.google.steps","automationcraft.engine.bdd"},
+        plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread"},
+        tags = ""
+)
+public class GRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider
+    //@DataProvider (parallel = true) -- For parallel execution support (which is not going to work for our code)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+}
---
+++ src/test/java/automationcraft/testcreation/google/features/googleSearch.feature
@@ -0,0 +1,15 @@
+Feature: Google Search
+  'When I go to the Google search page, and search for an item,
+  I expect to see some reference to that item in the result summary.'
+
+  Scenario: Standard Search One
+    Given that I have gone to the Google page
+    When I add "cats" to the search box
+    And click the Search Button
+    Then "cats" should be mentioned in the results
+
+  Scenario: Standard Search Two
+    Given that I have gone to the Google page
+    When I add "dogs" to the search box
+    And click the Search Button
+    Then "dogs" should be mentioned in the results
