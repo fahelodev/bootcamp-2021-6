23d234176736eff87e0a2b5392e397e0c01b23de
Diego.Yanez <diego.y8@gmail.com>
2022-01-11 20:54:54 -0300

    CasesGoogleAnimalsFrameworkCucumber

---
+++ src/test/java/automationcraft/testcreation/googleDyanez/pages/GooglePages.java
@@ -0,0 +1,70 @@
+package automationcraft.testcreation.googleDyanez.pages;
+
+import automationcraft.engine.selenium.SeleniumBaseDyanez;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GooglePages extends SeleniumBaseDyanez {
+
+
+
+    //Atraibutos o Localizaodres
+    private String searchTextField = "//*[@name='q']";
+    private String searchButton = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]";
+    private String searchResults = "//div[@class=\"g\"]//h3/a";
+    private String firtsResults = "//*[@id='rso']/div/div";
+
+    public GooglePages(WebDriver driver) {
+        super(driver);
+    }
+
+    // Metodos KeywordDriven
+
+
+    public void navigateToGoogle(){
+
+        navigateTo("https://www.google.cl");
+    }
+
+
+    public void enterSearchCriteria(String nameAnimal) throws InterruptedException {
+        writeTextWithEscape(searchTextField,nameAnimal);
+
+    }
+
+    public void clickGoogleSearch(){
+
+        clickElement(searchButton);
+    }
+
+    public String validateTextCart(){
+
+        return textFromElement(firtsResults);
+    }
+
+    public void validation(String inputText){
+        String result = getTitle();
+        Assert.assertTrue(result.contains(inputText));
+    }
+
+
+
+    public List<String> getAllSearchResults(){
+        List<WebElement> list = bringMeAllElements(searchResults);
+        List<String> stringFromList = new ArrayList<String>();
+        for(WebElement e : list){
+            stringFromList.add(e.getText());
+        }
+
+        return stringFromList;
+
+    }
+
+
+
+
+}
---
+++ src/test/java/automationcraft/testcreation/googleDyanez/steps/GoogleSearchSteps.java
@@ -0,0 +1,46 @@
+package automationcraft.testcreation.googleDyanez.steps;
+
+import automationcraft.engine.selenium.DriverFactory;
+import automationcraft.testcreation.google.pages.GooglePages;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+
+public class GoogleSearchSteps {
+
+    protected GooglePages google = new GooglePages(DriverFactory.getDriver());
+
+
+    @Given("that I have gone to the Google page")
+    public void that_i_have_gone_to_the_google_page() {
+
+        google.navigateToGoogle();
+    }
+
+    @When("I add {string} to the search box")
+    public void i_add_to_the_search_box(String nameAnimal) throws InterruptedException {
+
+        google.enterSearchCriteria(nameAnimal);
+
+    }
+
+    @When("click the Search Button")
+    public void click_the_search_button() {
+
+        google.clickGoogleSearch();
+    }
+
+
+    @Then("the {string} should be mentioned in the results")
+    public void should_be_mentioned_in_the_results(String nameAnimal) {
+
+        google = new GooglePages(DriverFactory.getDriver());
+        String searchResult = google.getTitle();
+            Assert.assertTrue(searchResult.contains(nameAnimal));
+
+
+    }
+
+
+}
