508971e549d39f6401330f95fcfdc1f4ac044786
Facundo Mercado <Facundo.Mercado@tsoftglobal.com>
2022-01-12 13:08:58 -0300

    correccion de 1 letra

--- src/test/java/automationcraft/delivery/runners/FMercado/FMRunner.java
+++ src/test/java/automationcraft/delivery/runners/FMercado/FMRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"src/test/java/automationcraft/testcreation/FMercado/features"},
+        features = {"src/test/java/automationcraft/testcreation/FMercado/feature"},
         glue = {"automationcraft.testcreation.FMercado.steps","automationcraft.engine.bdd"},
         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:test-output-thread"},
