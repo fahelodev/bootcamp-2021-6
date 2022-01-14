e8449b22ee1aec16be7a21f0364b82d05b0250ac
Nicolas Marinucci <Nicolas.Marinucci@tsoftglobal.com>
2022-01-14 09:48:31 -0300

    Refactor Covid19

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a6d98e43-33f2-4e6c-815a-b755893a5632",
+    "feature": "Carrito de Compras",
+    "scenario": "CA-01 Limite de Stock de un producto",
+    "start": 1642011505391,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1642011575564,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[TestNG-test\u003dSurefire test-1,5,main]"
+  }
+]);
+});
